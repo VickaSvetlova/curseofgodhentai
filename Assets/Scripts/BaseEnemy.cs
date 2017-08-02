c69ed5ecabfb167535f4d6c0995cfa48f4dfc288
Tory <vickasvetlova@gmail.com>
2017-08-02 14:27:31 +0300

    add enemy

---
+++ Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemy : MonoBehaviour 
+{
+	#region Enums
+    #endregion
+
+    #region Delegates
+    #endregion
+
+    #region Structures
+    #endregion
+
+    #region Classes
+    #endregion
+
+    #region Fields
+    #endregion
+
+    #region Events
+    #endregion
+
+    #region Properties
+    #endregion
+
+    #region Methods
+    #endregion
+
+    #region Event Handlers
+    #endregion
+}
